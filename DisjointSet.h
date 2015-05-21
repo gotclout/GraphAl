be35d91b1ebb08d8f6ac10bee6403578869e8b9a
General <rlfosterjr@gmail.com>
2015-05-21 16:49:53 -0400

    Update DisjointSet.h

--- DisjointSet.h
+++ DisjointSet.h
@@ -6,7 +6,7 @@
 using std::map;
 
 /**
- *
+ * Templated DisjointSet (Union Find) data structure
  */
 template <typename Thing>
 class DisjointSet
@@ -19,15 +19,31 @@ class DisjointSet
    * TODO: Probably make struct Set
    *       Consider Path Compression
    *       Union & Union By Rank
+   *       configurable
    */
   struct Set
   {
-    Thing value;  //
-    Set*  parent; //
-    int   rank;   //
+    Thing  value;  // Template element
+    Set*   parent; // Parent set (self if root)
+    int    rank;   // Rank for union operation
 
     /**
-     *
+     * Default Construct
+     */
+    Set() { parent = this; rank = 0; };
+
+    /**
+     * Construct from type
+     */
+    Set(const Thing & t) { value = t; parent = this; rank = 0; };
+
+    /**
+     * Less than operator overload for container compliance
+     */
+    bool operator<(const Set & rhs) { return value < rhs.value; };
+
+    /**
+     * Equivalence operator overload
      */
     bool operator==(const Set & rhs)
     {
@@ -35,22 +51,29 @@ class DisjointSet
     };
   };
 
-  typedef map<T, Set*> Forest;
-  typedef typename Forest::iterator ForestIt;
+  /**
+   * Wraper for std::map comparator map<T*, Set*, Compare> force value
+   * compare rather than pointer compare since T* is copied once in the
+   * Set struct. Avoid copying it into the map since T could be large
+   */
+  struct ThingPtrCompare
+  {
+    bool operator()(const T* lhs, const T* rhs) { return *lhs < *rhs; };
+  };
+
+  typedef map<T*, Set*, ThingPtrCompare> Forest;
+  typedef typename Forest::iterator      ForestIt;
 
   private:
 
-  Forest forest; // Map Typename to Set of O(1) lookups
+  Forest forest; // Map Typename to Set for O(1) lookups
 
   public:
 
   /**
    * Default constructor
    */
-  DisjointSet()
-  {
-    ;
-  };
+  DisjointSet() { ; };
 
   /**
    * Destructor
@@ -59,9 +82,8 @@ class DisjointSet
   {
     ForestIt i = forest.begin(),
              e = forest.end();
-    Set*     s = 0;
 
-    for( ; i != e; ++i)
+    for(Set* s = 0; i != e; ++i)
     {
       if(i->second)
       {
@@ -79,42 +101,35 @@ class DisjointSet
    */
   Set* MakeSet(const T & e)
   {
-    Set* ret    = new Set;
-    ret->value  = e;
-    ret->rank   = 0;
-    ret->parent = ret;
-    forest[e]   = ret;
+    Set* ret            = new Set(e);
+    forest[&ret->value] = ret;
 
     return ret;
   };
 
   /**
-   *
+   * With path compression
    */
   Set* FindSet(Set* & s)
   {
-    Set* ret = 0;
-
-    ret = *(s->parent) == *s ? s : FindSet(s->parent);
-
-    return ret;
+    return *(s->parent) == *s ? s : s->parent = FindSet(s->parent);
   };
 
   /**
-   *
+   * Find template element
    */
-  T* FindSet(const T & e)
+  T* Find(T & e)
   {
-    return forest.find(e) == forest.end() ? 0 : &FindSet(forest[e])->value;
+    return forest.find(&e) == forest.end() ? 0 : &FindSet(forest[&e])->value;
   };
 
   /**
-   *
+   * By rank
    */
   Set* Union(T & x, T & y)
   {
-    Set *xroot = FindSet(forest[x]),
-        *yroot = FindSet(forest[y]),
+    Set *xroot = FindSet(forest[&x]),
+        *yroot = FindSet(forest[&y]),
         *ret   = xroot;
 
     if(!(*xroot == *yroot))
@@ -139,12 +154,14 @@ class DisjointSet
   };
 
   /**
-   * Testing
+   * TODO: This is for testing
    */
-  const Forest& Get()
-  {
-    return forest;
-  };
+  const Forest& Get() { return forest; };
+
+  /**
+   * Retrieves the total number of unique elements in Forest
+   */
+  size_t size() { return forest.size(); };
 };
 
 #endif//__DisjointSet__
